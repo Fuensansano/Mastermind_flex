ab2cc2c1ec581265b69ae551740784fa2fcab66d
ivan3131411 <ivan@flexmyroom.com>
2023-10-18 17:15:41 +0200

    given a two color rigth combination should return [2,0]

--- spec/Mastemind_spec.rb
+++ spec/Mastemind_spec.rb
@@ -4,24 +4,24 @@ require_relative '../src/mastermind'
 describe Mastermind do
   let(:mastermind) { Mastermind.new }
 
-  it 'should return [1,0] when guesser guess the combination' do
+  it 'given a right guess combination should return [1,0] ' do
     guess_combination = ['blue']
     win_combination = ['blue']
 
     expect(mastermind.evaluate(win_combination, guess_combination)).to eq([1,0])
   end
 
-  it 'should return [0,0] when guesser dosnt guess the combination' do
+  it 'given a wrong guess combination should return [0,0]' do
     guess_combination = ['red']
     win_combination = ['blue']
 
     expect(mastermind.evaluate(win_combination, guess_combination)).to eq([0,0])
   end
 
-  it 'should return [0,0] when guesser combination is yellow and win combination is blue' do
-    guess_combination = ['yellow']
-    win_combination = ['blue']
+  it 'given a two color rigth combination should return [2,0]' do
+    guess_combination = ['red', 'blue']
+    win_combination = ['red', 'blue']
 
-    expect(mastermind.evaluate(win_combination, guess_combination)).to eq([0,0])
+    expect(mastermind.evaluate(win_combination, guess_combination)).to eq([2,0])
   end
 end
--- src/mastermind.rb
+++ src/mastermind.rb
@@ -3,6 +3,7 @@
 class Mastermind
   def evaluate(win_combination, guess_combination)
     return [0,0] if guess_combination.first != win_combination.first
+    return [2,0] if guess_combination.size > 1
 
     [1,0]
   end
